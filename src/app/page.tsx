f1c59de59bf20a9775b42af3ee98f7eb167e97ac
Giper8ola <matwey.peregudow2100@gmail.com>
2025-05-05 23:14:36 +0300

    lint init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from '../assets/css/example.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-
-    </div>
-  );
+	return <div className={styles.page}></div>;
 }
